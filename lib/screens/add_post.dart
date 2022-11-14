30714461e7b22d75caf259d16613c4d862a1b162
Usaid Asif <usaidasif12@gmail.com>
2022-11-14 14:59:24 +0500

    Added data on firebase

--- lib/screens/add_post.dart
+++ lib/screens/add_post.dart
@@ -1,8 +1,14 @@
 import 'dart:io';
 
 import 'package:blogapp/components/round_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:firebase_database/firebase_database.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -12,6 +18,14 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
+  bool showSpinner = false;
+
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  final postRef = FirebaseDatabase.instance.reference().child('Posts');
+  firebase_storage.FirebaseStorage storage =
+      firebase_storage.FirebaseStorage.instance;
+
   File? _image;
   final picker = ImagePicker();
 
@@ -81,96 +95,167 @@ class _AddPostScreenState extends State<AddPostScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Upload Blog'),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: Column(
-            children: [
-              InkWell(
-                onTap: () {
-                  dialog(context);
-                },
-                child: Center(
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * .2,
-                    width: MediaQuery.of(context).size.width * 1,
-                    child: _image != null
-                        ? ClipRRect(
-                            child: Image.file(
-                              _image!.absolute,
+    return ModalProgressHUD(
+      inAsyncCall: showSpinner,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Upload Blog'),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: Column(
+              children: [
+                InkWell(
+                  onTap: () {
+                    dialog(context);
+                  },
+                  child: Center(
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * .2,
+                      width: MediaQuery.of(context).size.width * 1,
+                      child: _image != null
+                          ? ClipRRect(
+                              child: Image.file(
+                                _image!.absolute,
+                                width: 100,
+                                height: 100,
+                                fit: BoxFit.fill,
+                              ),
+                            )
+                          : Container(
+                              decoration: BoxDecoration(
+                                color: Colors.grey.shade200,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
                               width: 100,
                               height: 100,
-                              fit: BoxFit.fill,
+                              child: const Icon(
+                                Icons.camera_alt,
+                                color: Colors.blue,
+                              ),
                             ),
-                          )
-                        : Container(
-                            decoration: BoxDecoration(
-                              color: Colors.grey.shade200,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            width: 100,
-                            height: 100,
-                            child: const Icon(
-                              Icons.camera_alt,
-                              color: Colors.blue,
-                            ),
-                          ),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Form(
-                child: Column(
-                  children: [
-                    TextFormField(
-                      controller: tilteController,
-                      keyboardType: TextInputType.text,
-                      decoration: const InputDecoration(
-                        labelText: 'Title',
-                        hintText: 'Enter post title',
-                        border: OutlineInputBorder(),
-                        hintStyle: TextStyle(
-                            color: Colors.grey, fontWeight: FontWeight.normal),
-                        labelStyle: TextStyle(
-                            color: Colors.grey, fontWeight: FontWeight.normal),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    TextFormField(
-                      controller: descriptionController,
-                      keyboardType: TextInputType.text,
-                      minLines: 1,
-                      maxLines: 5,
-                      decoration: const InputDecoration(
-                        labelText: 'Description',
-                        hintText: 'Enter post description',
-                        border: OutlineInputBorder(),
-                        hintStyle: TextStyle(
-                            color: Colors.grey, fontWeight: FontWeight.normal),
-                        labelStyle: TextStyle(
-                            color: Colors.grey, fontWeight: FontWeight.normal),
-                      ),
-                    ),
-                  ],
+                const SizedBox(
+                  height: 30,
                 ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              RoundButton(title: 'Upload', onPress: () {})
-            ],
+                Form(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: tilteController,
+                        keyboardType: TextInputType.text,
+                        decoration: const InputDecoration(
+                          labelText: 'Title',
+                          hintText: 'Enter post title',
+                          border: OutlineInputBorder(),
+                          hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.normal),
+                          labelStyle: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      TextFormField(
+                        controller: descriptionController,
+                        keyboardType: TextInputType.text,
+                        minLines: 1,
+                        maxLines: 5,
+                        decoration: const InputDecoration(
+                          labelText: 'Description',
+                          hintText: 'Enter post description',
+                          border: OutlineInputBorder(),
+                          hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.normal),
+                          labelStyle: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                RoundButton(
+                  title: 'Upload',
+                  onPress: () async {
+                    setState(() {
+                      showSpinner = true;
+                    });
+
+                    try {
+                      int date = DateTime.now().microsecondsSinceEpoch;
+
+                      firebase_storage.Reference ref = firebase_storage
+                          .FirebaseStorage.instance
+                          .ref('/blogapp$date');
+
+                      UploadTask uploadTask = ref.putFile(_image!.absolute);
+
+                      await Future.value(uploadTask);
+
+                      var newUrl = await ref.getDownloadURL();
+                      final User? user = _auth.currentUser;
+                      postRef.child('Post List').child(date.toString()).set({
+                        'pId': date.toString(),
+                        'pImage': newUrl.toString(),
+                        'pTime': date.toString(),
+                        'pTitle': tilteController.text.toString(),
+                        'pDescription': descriptionController.text.toString(),
+                        'uEmail': user!.email.toString(),
+                        'uId': user!.uid.toString(),
+                      }).then(
+                        (value) {
+                          toastMessage('Post Published');
+                          setState(() {
+                            showSpinner = false;
+                          });
+                        },
+                      ).onError(
+                        (error, stackTrace) {
+                          toastMessage(error.toString());
+                          setState(() {
+                            showSpinner = false;
+                          });
+                        },
+                      );
+                    } catch (e) {
+                      setState(
+                        () {
+                          showSpinner = false;
+                        },
+                      );
+
+                      toastMessage(e.toString());
+                    }
+                  },
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  void toastMessage(String message) {
+    Fluttertoast.showToast(
+        msg: message.toString(),
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.SNACKBAR,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.white,
+        textColor: Colors.black,
+        fontSize: 16.0);
   }
 }
